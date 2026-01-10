b8c647146b0b846ef50b5a2d7a5292bfe37e6460
Aaryav Taparia <aaryav.taparia@gmail.com>
2026-01-09 17:32:03 -0800

    Changes from Aaryav's laptop

--- fll-github-sync.cmd
+++
@@ -1,7 +0,0 @@
-cd C:\Users\sande\code\FLL-Unearthed-2025-2026
-copy "C:\Users\sande\Documents\LEGO Education SPIKE\FLL Unearthed\*.*" .
-git pull origin main
-git add .
-git commit -m "Describe your changes clearly"
-git pull origin main --rebase
-git push origin main
